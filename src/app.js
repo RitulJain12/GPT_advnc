6c23f29d7d5b5864fd2c8d082d5f5ceb8a7a329d
Ritul Jain <rituljain202@gmail.com>
2025-12-13 20:04:37 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const authRoutes=require('./routes/authRoutes');
 const chatRoutes=require('../src/routes/ChatRoute');
 const cors=require('cors');
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://gpt-advnc-1.onrender.com",
     credentials: true, 
 }))
 
